7004d629485c05f20798623ff2d3da2ebe8573fd
juanp <juanpablohdzm95@gmail.com>
2022-03-29 07:16:55 -0600

    inline styling.

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ margin: 16, borderWidth: 3, borderColor: "red", padding: 16 }}>
+        Hello World!
+      </Text>
+      <Button title="click me!" />
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
