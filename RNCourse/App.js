4092c10ff3d103138e0af176e85396705871317a
juanp <juanpablohdzm95@gmail.com>
2022-03-29 07:22:01 -0600

    object styling

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -3,13 +3,18 @@ import { StyleSheet, Text, View, Button } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ margin: 16, borderWidth: 3, borderColor: "red", padding: 16 }}>
-        Hello World!
-      </Text>
+      <View>
+        <Text style={customStyle.dummyText}>Another piece of text</Text>
+      </View>
+      <Text style={customStyle.dummyText}>Hello World!</Text>
       <Button title="click me!" />
     </View>
   );
 }
+
+const customStyle = StyleSheet.create({
+  dummyText: { margin: 16, borderWidth: 3, borderColor: "red", padding: 16 },
+});
 
 const styles = StyleSheet.create({
   container: {
