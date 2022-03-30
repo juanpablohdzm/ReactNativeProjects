f886d133fddaa75a41c086da9822f57835ee90bc
juanp <juanpablohdzm95@gmail.com>
2022-03-30 08:59:21 -0600

    configuring the text input field.

--- GuessNumberGame/components/screens/StartGameScreen.js
+++ GuessNumberGame/components/screens/StartGameScreen.js
@@ -3,8 +3,14 @@ import PrimaryButton from "../PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View style= {styles.inputContainer}>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType={"number-pad"}
+        autoCapitalize="none"
+        autoCorrect = {false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -17,12 +23,23 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     padding: 16,
     borderRadius: 8,
-    backgroundColor: '#72063c',
+    backgroundColor: "#72063c",
     elevation: 4, //Android specific shadow
     //iOS specific shadow
-    shadowColor: 'black',
-    shadowOffset: {width:0, height:2},
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
   },
 });
