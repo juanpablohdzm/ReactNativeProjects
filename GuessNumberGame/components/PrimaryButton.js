de0bb4ac30ca3a42bf83712d30aafd5880a0fcd3
juanp <juanpablohdzm95@gmail.com>
2022-03-30 09:47:08 -0600

    improving the buttons using layout flex.

--- GuessNumberGame/components/PrimaryButton.js
+++ GuessNumberGame/components/PrimaryButton.js
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export default function PrimaryButton(props) {
   return (
-    <View>
-      <Text>{props.children}</Text> 
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+        onPress={() => console.log("hello")}
+        android_ripple={{ color: "#690437" }}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 ///Children is the text between tags <>Hello</> children = hello
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- GuessNumberGame/components/screens/StartGameScreen.js
+++ GuessNumberGame/components/screens/StartGameScreen.js
@@ -9,27 +9,41 @@ export default function StartGameScreen() {
         maxLength={2}
         keyboardType={"number-pad"}
         autoCapitalize="none"
-        autoCorrect = {false}
+        autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center", //Main axis in this case vertical
+    alignItems: "center", //Secondary axis in this case horizontal
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
     borderRadius: 8,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     elevation: 4, //Android specific shadow
     //iOS specific shadow
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  buttonsContainer: {
+    flexDirection: "row-reverse",
+  },
+  buttonContainer: {
+    flex: 1,
   },
   numberInput: {
     height: 50,
