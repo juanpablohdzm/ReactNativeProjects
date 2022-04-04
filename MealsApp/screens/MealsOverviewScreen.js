c24bee536822a1dd70cef498308e8abfde5dd6f2
Juan Pablo Hernandez Mosti <>
2022-04-04 06:52:48 -0500

    configuration style of nav screens

--- MealsApp/screens/MealsOverviewScreen.js
+++ MealsApp/screens/MealsOverviewScreen.js
@@ -1,14 +1,24 @@
-import { View, FlatList, StyleSheet } from 'react-native';
+import { useLayoutEffect } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
 
-import MealItem from '../components/MealItem';
-import { MEALS } from '../data/dummy-data';
+import MealItem from "../components/MealItem";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => { ///Happens simultaneously when rendering this component
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, []);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
@@ -18,11 +28,9 @@ function MealsOverviewScreen({ route }) {
       imageUrl: item.imageUrl,
       affordability: item.affordability,
       complexity: item.complexity,
-      duration: item.duration
+      duration: item.duration,
     };
-    return (
-      <MealItem {...mealItemProps} />
-    );
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
