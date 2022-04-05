4ef74a72ac08975464c9a19839c89d063b903080
Juan Pablo Hernandez Mosti <>
2022-04-05 10:52:11 -0500

    Add expense tracker project

---
+++ ExpenseTracker/components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,92 @@
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import Button from "../UI/Button";
+
+import Input from "./Input";
+export default function ExpenseForm({ submitButtonLabel, onCancel, onSubmit }) {
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+  function inputChangeHandler(inputIdentifier, enteredValue) {
+    setAmountValue((curInputValues) => {
+      return {
+        ...curInputValues,
+        [inputIdentifier]: enteredValue, //Dynamic target property
+      };
+    });
+  }
+
+  function submitHandler() {}
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: inputChangeHandler.bind(this, "amount"),
+            value: inputValues.amount,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: inputChangeHandler.bind(this, "date"),
+            value: inputValues.amount,
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          onChangeText: inputChangeHandler.bind(this, "amount"),
+          value: inputValues.amount,
+        }}
+      />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 24,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
