2cb204adaff548e0a736000268cfca39b3b9424d
Juan Pablo Hernandez Mosti <>
2022-04-07 07:11:53 -0500

    Add new project.

--- AuthApp/screens/LoginScreen.js
+++ AuthApp/screens/LoginScreen.js
@@ -1,7 +1,24 @@
 import AuthContent from '../components/Auth/AuthContent';
+import {useState} from 'react';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+
+import {login} from '../util/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function loginHandler({email, password}){
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if(isAuthenticating){
+    return <LoadingOverlay message='Loggin user...'/>
+  }
+
+  return <AuthContent isLogin  onAuthenticate={loginHandler}/>;
 }
 
 export default LoginScreen;
