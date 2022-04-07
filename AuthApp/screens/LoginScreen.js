b4aa0ca42779b5f20f19557f3a4f3b86f1b20fe6
Juan Pablo Hernandez Mosti <>
2022-04-07 10:48:33 -0500

    Get login token

--- AuthApp/screens/LoginScreen.js
+++ AuthApp/screens/LoginScreen.js
@@ -3,15 +3,24 @@ import {useState} from 'react';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 import {login} from '../util/auth';
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
 
-  const [isAuthenticating, setIsAuthenticating] = useState(false)
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
 
   async function loginHandler({email, password}){
     setIsAuthenticating(true);
-    await login(email, password);
-    setIsAuthenticating(false);
+    try{
+      const token = await login(email, password);
+      authCtx.authenticate(token); 
+    }catch(error){
+      Alert.alert('Authentication failed!','Could not log in. Please check your credentials or try again later!');
+      setIsAuthenticating(false);
+    }
   }
 
   if(isAuthenticating){
--- AuthApp/screens/SignupScreen.js
+++ AuthApp/screens/SignupScreen.js
@@ -1,23 +1,30 @@
-import AuthContent from '../components/Auth/AuthContent';
-import { useState } from 'react';
-import { createUser } from '../util/auth';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { useState , useContext} from "react";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  const [isAuthenticating, setIsAuthenticating] = useState(false)
+  const authCtx = useContext(AuthContext);
 
-  async function signupHandler({email, password}){
+  async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
-    setIsAuthenticating(false);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Authentication failed", "Could not create user");
+      setIsAuthenticating(false);
+    }
   }
 
-  if(isAuthenticating){
-    return <LoadingOverlay message='Creating user...'/>
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
   }
 
-  return <AuthContent onAuthenticate={signupHandler}/>;
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
