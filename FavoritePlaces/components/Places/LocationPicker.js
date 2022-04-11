c3529309c9fe6639fd80f84dd9618b9fef33b770
Juan Pablo Hernandez Mosti <>
2022-04-11 18:37:45 +0200

    expo location

---
+++ FavoritePlaces/components/Places/LocationPicker.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, View } from "react-native";
+import OutlinedButton from "../UI/OutlinedButton";
+
+import { getCurrentPositionAsync, useForegroundPermissions } from "expo-location";
+
+export default function LocationPicker() {
+
+const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+
+
+ async function verifyPermissions(){
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+        const permissionResponse = await requestPermission();
+        return permissionResponse.granted;
+      }
+  
+      if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+        Alert.alert(
+          "Insufficient Permissions!",
+          "You need to grant camera permissions to use this app."
+        );
+        return false;
+      }
+  
+      return true;
+ }
+  async function getLocationHandler() {
+
+    const hasPermission = await verifyPermissions();
+
+    if(!hasPermission){
+        return;
+    }
+       const location = await  getCurrentPositionAsync();
+  }
+
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.input}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
